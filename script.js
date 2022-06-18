825b44dec6ce933dd478a247132fc1ed4e311a46
andrglad <81357549+andrglad@users.noreply.github.com>
2022-06-18 13:18:23 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-
+document.getElementById("upper-case").addEventListener("click", function () {
+    let text = document.getElementById("textarea").value;
+    let conv = text.toUpperCase();
+    document.getElementById("textarea").value = conv
+})
+document.getElementById("lower-case").addEventListener("click", function () {
+    let text = document.getElementById("textarea").value;
+    let conv = text.toLowerCase();
+    document.getElementById("textarea").value = conv
+})
+document.getElementById("proper-case").addEventListener("click", function () {
+    let text = document.getElementById("textarea").value;
+    let conv = text.toLowerCase();
+    let prop = conv.replace(/^(.)|\s(.)/g, function ($1) {return $1.toUpperCase()});
+    document.getElementById("textarea").value = prop
+})
+document.getElementById("sentence-case").addEventListener("click", function () {
+    let text = document.getElementById("textarea").value;
+    let conv = text.toLowerCase();
+    let prop = conv.replace(/^(.)|\.\s(.)/g, function ($1) {return $1.toUpperCase()});
+    document.getElementById("textarea").value = prop
+})
